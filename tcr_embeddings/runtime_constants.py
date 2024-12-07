6f4ed92dd82348f1f0996d2b22a5f6a4289c7507
RcwYuen <zcabcry@ucl.ac.uk>
2024-12-07 21:30:02 +0000

    Update runtime_constants.py

--- tcr_embeddings/runtime_constants.py
+++ tcr_embeddings/runtime_constants.py
@@ -1,3 +1,4 @@
 from pathlib import Path
 
-HOME_PATH: Path = Path.cwd().parent
+HOME_PATH: Path = Path(__file__).resolve().parent.parent
+DATA_PATH: Path = Path(__file__).resolve().parent.parent / "data"
