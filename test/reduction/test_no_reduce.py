8610bcebeb3906c381a635ff9d104a8c6bea6911
RcwYuen <zcabcry@ucl.ac.uk>
2024-12-07 18:50:13 +0000

    Unittests for Reduction Method Functions
    Adding unit tests for reduction methods (specifically NoReduce and JL) - reduction.py has passed the test for these two

---
+++ test/reduction/test_no_reduce.py
@@ -0,0 +1,42 @@
+import unittest
+import numpy as np
+from tcr_embeddings.reduction.reduction import NoReduce
+import torch
+
+class test_no_reduce(unittest.TestCase):
+    LS_DIMS: list = [10, 14, 16, 64, 768]
+
+    def test_norm_of_reduced_ndarray_is_one(self):
+        for in_dim in test_no_reduce.LS_DIMS:
+            reduction_method = NoReduce()
+            ls_reduced = reduction_method.reduce(np.random.normal(0, 1, size = (100, in_dim)))
+            ls_diffs = abs(np.linalg.norm(ls_reduced, axis=1) - 1).tolist()
+            for val in ls_diffs:
+                self.assertTrue(val < 0.001)
+
+    def test_norm_of_reduced_tensor_is_one(self):
+        for in_dim in test_no_reduce.LS_DIMS:
+            reduction_method = NoReduce()
+            ls_vecs = np.random.normal(0, 1, size = (100, in_dim))
+            ls_reduced = reduction_method.reduce(torch.from_numpy(ls_vecs))
+            ls_diffs = abs(np.linalg.norm(ls_reduced, axis=1) - 1).tolist()
+            for val in ls_diffs:
+                self.assertTrue(val < 0.001)
+
+    def test_ndarray_norming_right_axis(self):
+        for in_dim in test_no_reduce.LS_DIMS:
+            reduction_method = NoReduce()
+            ls_reduced = reduction_method.reduce(np.random.normal(0, 1, size = (100, in_dim)))
+            ls_diffs = abs(np.linalg.norm(ls_reduced, axis=1) - 1).tolist()
+            self.assertTrue(len(ls_diffs), 100)
+
+    def test_tensor_norming_right_axis(self):
+        for in_dim in test_no_reduce.LS_DIMS:
+            reduction_method = NoReduce()
+            ls_vecs = np.random.normal(0, 1, size = (100, in_dim))
+            ls_reduced = reduction_method.reduce(torch.from_numpy(ls_vecs))
+            ls_diffs = abs(np.linalg.norm(ls_reduced, axis=1) - 1).tolist()
+            self.assertTrue(len(ls_diffs), 100)
+
+if __name__ == '__main__':
+    unittest.main()
