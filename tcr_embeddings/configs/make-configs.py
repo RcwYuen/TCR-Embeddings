2f3e96d68ea91593c166e0173d20a19e25ebef41
RcwYuen <zcabcry@ucl.ac.uk>
2024-12-09 00:10:06 +0000

    Update make-configs.py

--- tcr_embeddings/configs/make-configs.py
+++ tcr_embeddings/configs/make-configs.py
@@ -1,7 +1,9 @@
 import json
 from pathlib import Path
 
-dir = Path(__file__).resolve().parent
+from tcr_embeddings.training.training_utils import load_default_configs
+
+loc = Path(__file__).resolve().parent
 
 encodings = [
     "atchley",
@@ -13,22 +15,21 @@ encodings = [
     "sceptr-tiny",
 ]
 kfolds = 5
-config_path_base = Path.cwd() / "configs/_config.json"
 
-with open(dir / config_path_base, "r") as file:
-    config = json.load(file)
+if __name__ == "__main__":
+    config = load_default_configs()
 
-for encoding in encodings:
-    for fold in range(kfolds):
-        config["encoding"] = encoding
-        config["kfold"] = fold
-        config["output-path"] = f"results/{encoding}-no-reduction/kfold-{fold}"
-        config["reduction"] = ""
-        with open(dir / f"{encoding}-kfold-{fold}-no-reduction.json", "w") as f:
-            f.write(json.dumps(config, indent=4))
+    for encoding in encodings:
+        for fold in range(kfolds):
+            config["encoding"] = encoding
+            config["kfold"] = fold
+            config["output-path"] = f"results/{encoding}-no-reduction/kfold-{fold}"
+            config["reduction"] = ""
+            with open(loc / f"{encoding}-kfold-{fold}-no-reduction.json", "w") as f:
+                f.write(json.dumps(config, indent=4))
 
-        config["reduction"] = "autoencoder"
-        config["output-path"] = f"results/{encoding}-autoencoder/kfold-{fold}"
+            config["reduction"] = "autoencoder"
+            config["output-path"] = f"results/{encoding}-autoencoder/kfold-{fold}"
 
-        with open(dir / f"{encoding}-kfold-{fold}-autoencoder.json", "w") as f:
-            f.write(json.dumps(config, indent=4))
+            with open(loc / f"{encoding}-kfold-{fold}-autoencoder.json", "w") as f:
+                f.write(json.dumps(config, indent=4))
