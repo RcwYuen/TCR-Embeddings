660d6361c304ff23fa1214e9b303cfd95e24216c
RcwYuen <zcabcry@ucl.ac.uk>
2024-12-16 21:38:10 +0000

    Delete poetry_fill.py

--- utils/poetry_fill.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-with open("requirements.txt") as f:
-    for line in f:
-        package = line.strip()
-        if package:
-            subprocess.run(["poetry", "add", package])
