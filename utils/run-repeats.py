84f5253e221d1deb968a9cf5bbf02f7a5c159617
RcwYuen <zcabcry@ucl.ac.uk>
2024-12-27 21:50:16 +0000

    Update run-repeats.py

--- utils/run-repeats.py
+++ utils/run-repeats.py
@@ -13,13 +13,11 @@ kfolds = 5
 
 for i in range(kfolds):
     for encoding in encodings:
-        fname = f"{encoding}-kfold-{i}.json"
-        working_dir = "/cs/student/projects1/2020/cheuyuen/"
-        command = f"python training/trainer.py -c {fname}\n"
+        fname = f"{encoding}-kfold-{i}.json"  # change to your config names
+        working_dir = "path/to/your/working/directory"
+        command = f"python -m tcr_embeddings.trainer -c {fname}\n"
         screen_start_command = f"screen -dmS {encoding}_{i}"
         subprocess.run(screen_start_command, shell=True, check=True)
-        full_command = (
-            f"cd {working_dir}; source tcr-embedding/bin/activate.csh; {command}"
-        )
+        full_command = f"cd {working_dir}; conda activate tcr_embeddings; {command}"
         screen_stuff_command = f'screen -S {encoding}_{i} -X stuff "{full_command}"'
         subprocess.run(screen_stuff_command, shell=True, check=True)
