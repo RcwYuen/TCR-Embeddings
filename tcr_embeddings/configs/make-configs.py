b250646939849fda743101082588970fbca3e8d6
RcwYuen <rcwyuen@gmail.com>
2024-12-28 18:51:59 +0000

    Uploading Configurations

--- tcr_embeddings/configs/make-configs.py
+++ tcr_embeddings/configs/make-configs.py
@@ -1,35 +1,30 @@
 import json
 from pathlib import Path
 
-from tcr_embeddings.training.training_utils import load_default_configs
+from tcr_embeddings.training.training_utils import (
+    load_default_configs,
+    make_directory_where_necessary,
+)
 
 loc = Path(__file__).resolve().parent
 
-encodings = [
-    "atchley",
-    "kidera",
-    "rand",
-    "aaprop",
-    "tcrbert",
-    "sceptr-default",
-    "sceptr-tiny",
-]
+ls_encodings = ["atchley", "kidera", "rand", "aaprop", "sceptr-default", "sceptr-tiny"]
+ls_reduction_methods = ["johnson-lindenstarauss", "autoencoder", "no-reduction"]
 kfolds = 5
 
 if __name__ == "__main__":
     config = load_default_configs()
 
-    for encoding in encodings:
+    for encoding in ls_encodings:
         for fold in range(kfolds):
-            config["encoding"] = encoding
-            config["kfold"] = fold
-            config["output-path"] = f"results/{encoding}-no-reduction/kfold-{fold}"
-            config["reduction"] = ""
-            with open(loc / f"{encoding}-kfold-{fold}-no-reduction.json", "w") as f:
-                f.write(json.dumps(config, indent=4))
+            for reduction in ls_reduction_methods:
+                store_location = make_directory_where_necessary(
+                    loc / f"{encoding}/{reduction}"
+                )
+                config["encoding"] = encoding
+                config["kfold"] = fold
+                config["output-path"] = f"results/{encoding}-{reduction}/kfold-{fold}"
+                config["reduction"] = reduction
 
-            config["reduction"] = "autoencoder"
-            config["output-path"] = f"results/{encoding}-autoencoder/kfold-{fold}"
-
-            with open(loc / f"{encoding}-kfold-{fold}-autoencoder.json", "w") as f:
-                f.write(json.dumps(config, indent=4))
+                with open(store_location / f"kfold-{fold}.json", "w") as f:
+                    f.write(json.dumps(config, indent=4))
